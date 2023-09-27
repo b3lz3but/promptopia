56acea67de7e0c3796dc954236ad36a3f0c788e7
Ciprian <craps2003@gmail.com>
2023-09-27 17:58:20 +0300

    title & subtitle

--- app/page.jsx
+++ app/page.jsx
@@ -2,11 +2,18 @@ import React from "react";
 
 const Home = () => {
     return (
-        <div>
-            <h1>Home</h1>
-        </div>
-    )
-}
+        <section className="w-full flex-center flex-col">
+            <h1 className="head_text orange_gradient text-center">
+                Discover & Share
+                <br className="max-md:hidden" />
+                <span className="blue_gradient text-center">AI-Powered Prompts</span>
+            </h1>
+            <p className="desc text-center">Promptopia is a sophisticated, open-source AI tool, ingeniously designed for the contemporary world. It empowers users to discover, curate, and share inventive prompts, fostering a vibrant crossroads of creativity and technology.</p>
+        
+        {/* Feed */}
+       </section>
+    );
+};
 
 export default Home;
 
