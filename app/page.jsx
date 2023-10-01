b13d7dac303f0c4be9c08d7892080ed12a9723f5
b3lz3but <craps2003@gmail.com>
2023-10-01 16:32:22 +0300

    fixed nav.jsx and page.jsx errors

--- app/page.jsx
+++ app/page.jsx
@@ -1,19 +1,17 @@
 import Feed from "@components/Feed";
 
-const Home = () => {
-    return (
-        <section className="w-full flex-center flex-col">
-            <h1 className="head_text orange_gradient text-center">
-                Discover & Share
-                <br className="max-md:hidden" />
-                <span className="blue_gradient text-center">AI-Powered Prompts</span>
-            </h1>
-            <p className="desc text-center">Promptopia is a sophisticated, open-source AI tool, ingeniously designed for the contemporary world. It empowers users to discover, curate, and share inventive prompts, fostering a vibrant crossroads of creativity and technology.</p>
-   
-            <Feed />
-       </section>
-    );
-};
+const Home = () => (
+  <section className='w-full flex-center flex-col'>
+    <h1 className='orange_gradient head_text text-center'>
+      Discover & Share
+      <br className='max-md:hidden' />
+      <span className='blue_gradient text-center'> AI-Powered Prompts</span>
+    </h1>
+    <p className='desc text-center'>
+    Promptopia is a sophisticated, open-source AI tool, ingeniously designed for the contemporary world. It empowers users to discover, curate, and share inventive prompts, fostering a vibrant crossroads of creativity and technology.
+    </p>
+
+  </section>
+);
 
 export default Home;
-
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,4 +1,4 @@
-"use client";
+"use client"
 
 import Link from 'next/link';
 import Image from 'next/image';
@@ -8,9 +8,20 @@ import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 const Nav = () => {
     const isUserLoggedIn = true;
 
+    const [providers, setProviders] = useState(null);
+
+    useEffect(() => {
+        const getProviders = async () => {
+            const response = await getProviders();
+            setProviders(response);
+        };
+
+        getProviders();
+    }, []);
+
     return (
         <nav className='flex-between w-full mb-16 pt-3'>
-            <Link href='/' className='flex gap-2 flex-center'>
+            <Link href='/layout' className='flex gap-2 flex-center'>
                 <Image
                     src='/assets/images/logo.svg'
                     alt='logo'
@@ -26,19 +37,55 @@ const Nav = () => {
             <div className='sm:flex hidden'>
                 {isUserLoggedIn ? (
                     <div className='flex gap-3 md:gap-5'>
-                        <Link href='/create-prompt'>
-                            <a className='blue_btn'>Create Post</a>
+                        <Link href='/create-prompt' className='black_btn'>
+                            Create Post
                         </Link>
 
                         <button type='button' onClick={signOut} className='outline_btn'>
                             Sign Out
                         </button>
+
+                        <Link href="/profile">
+                            <Image 
+                                src="/assets/images/logo.svg"
+                                width={37}
+                                height={37}
+                                className='rounded-full'
+                                alt='Profile'
+                            />
+                        </Link>
                     </div>
                 ): (
                   <>
+                    {providers &&
+                        Object.values(providers).map((provider) => (
+                            <button
+                                type="button"
+                                key={provider.name}
+                                onClick={() => signIn(provider.id)}
+                                className='black_btn'
+                            >
+                                Sign In
+                            </button>
+                        ))} 
                   </>  
                 )}
-            </div>       
+            </div>
+
+            {/* Mobile Navigation*/}
+            <div className='sm:hidden flex relative'>
+                {isUserLoggedIn && (
+                    <div className="flex">
+                        <Image
+                        src="/assets/images/logo.svg"
+                    width={37}
+                    height={37}
+                    className="rounded-full"
+                    alt="Profile" 
+                        />
+                    </div>
+                    )}
+            </div>        
         </nav>
     )
 };
